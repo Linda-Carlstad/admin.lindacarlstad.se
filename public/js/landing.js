78f4ed0ae738bcbfa57b5cca49491ddf44f66852
Felix Wetell <felix.wetell@gmail.com>
2019-08-23 12:23:26 +0200

    Closes #27, reworked landing page

---
+++ public/js/landing.js
@@ -0,0 +1,12 @@
+$( document ).ready( function()
+{
+    var typed = new Typed( "#landing-message",
+    {
+        strings:
+        [
+            'Welcome to Linda Carlstads admin panel. ^1000 <br> If not authorised, please leave.',
+        ],
+        typeSpeed: 80,
+        showCursor: false,
+    } );
+} );
