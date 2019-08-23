3396649e1f9f25cab84de9b4a9754184203acfd3
Felix Wetell <felix.wetell@gmail.com>
2019-08-23 13:02:47 +0200

    Closes #21

--- app/BoardMember.php
+++ app/BoardMember.php
@@ -9,32 +9,39 @@ class BoardMember extends Model
 {
     public function create( Request $request )
     {
-        dd( $request );
         BoardMember::validateRequest( $request );
         if( empty( $request->image ) )
         {
-            $imageName = 'img/logo.png';
+            $image = 'img/logo.png';
         }
         else
         {
-            $imageName = Partner::setImageName( $request );
+            $image = Partner::setImageName( $request );
         }
         $boardMember = new BoardMember;
-        BoardMember::addValuesToObject( $boardMember, $request, $imageName );
+        BoardMember::addValuesToObject( $boardMember, $request, $image );
     }
 
     public static function updateInfo( BoardMember $boardMember, Request $request )
     {
         BoardMember::validateRequest( $request );
-        if( empty( $request->image ) )
+        if( empty( $boardMember->image ) )
         {
-            $imageName = 'img/logo.png';
+            if( empty( $request->image ) )
+            {
+                $image = '/img/logo.png';
+            }
+            else
+            {
+                $image = BoardMember::setImageName( $request );
+            }
         }
         else
         {
-            $imageName = Partner::setImageName( $request );
+            $image = $partner->image;
         }
-        BoardMember::addValuesToObject( $boardMember, $request, $imageName );
+
+        BoardMember::addValuesToObject( $boardMember, $request, $image );
     }
 
     public static function validateRequest( Request $request )
@@ -54,15 +61,15 @@ class BoardMember extends Model
         $name = str_slug( $request->title ) . '-' . time() . '.' . $request->image->getClientOriginalExtension();
         $folder = '/img/board/';
         $request->image->move( public_path( $folder ), $name );
-        $imageName = $folder . $name;
-        return $imageName;
+        $image = $folder . $name;
+        return $image;
     }
 
-    public static function addValuesToObject( BoardMember $boardMember, Request $request, $imageName )
+    public static function addValuesToObject( BoardMember $boardMember, Request $request, $image )
     {
         $boardMember->title = $request->title;
         $boardMember->name = $request->name;
-        $boardMember->image = $imageName;
+        $boardMember->image = $image;
         $boardMember->email = $request->email;
         $boardMember->description = $request->description;
         $boardMember->order = $request->order;
