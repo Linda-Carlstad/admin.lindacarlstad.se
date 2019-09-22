4ae10a13cb90ac55afd9547f7c3868b5344ed219
Felix Wetell <felix.wetell@gmail.com>
2019-09-22 22:42:40 +0200

    User can now search partners and have them paginated

--- resources/views/partner/index.blade.php
+++ resources/views/partner/index.blade.php
@@ -23,14 +23,36 @@
         <h1>Partners</h1>
         <hr>
         <a class="btn btn-primary m-1" href="{{ route( 'partner.create' ) }}">Lägg till partner</a>
-    </div>
+        @if( !$partners->isEmpty() )
+            <form class="col-md-8 offset-md-2 mt-2" action="{{ '/partner' }}" method="get">
+                @csrf
 
+                <div class="form-group row">
+                    <div class="input-group">
+                        <input id="search" type="text" placeholder="Sök.." class="form-control{{ $errors->has('search') ? ' is-invalid' : '' }}" name="search" value="{{ isset( $search ) ? $search : "" }}" autofocus>
+                        @if ($errors->has('search'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('search' ) }}</strong>
+                            </span>
+                        @endif
+                        <div class="input-group-append">
+                            <button type="submit" name="button" class="btn btn-primary btn-file">Sök</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        @endif
+    </div>
+    @if( isset( $search ) )
+        <h4>Du sökte på: <i>{{ $search }}</i></h4>
+    @endif
     @if( $partners->isEmpty() )
         <p class="text-center">
             Inga partner tillagda, lägg till en nu!
         </p>
+
     @else
-        <h4 class="text-center">Antal: {{ $partners->count() }}</h4>
+        <h4 class="text-center">Antal: {{ $total }}</h4>
         <table class="table table-hover">
             <thead class="thead-dark">
                 <tr>
