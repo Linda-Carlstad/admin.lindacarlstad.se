4025c335fde06538f47085b822fdb2d7fceea3db
Felix Wetell <felix.wetell@gmail.com>
2019-12-17 12:40:06 +0100

    Update

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+if( mix.inProduction() )
+{
+    mix.js('resources/js/app.js', 'public/js')
+        .sass('resources/sass/app.scss', 'public/css').version();
+}
+else
+{
+    mix.js('resources/js/app.js', 'public/js')
+        .sass('resources/sass/app.scss', 'public/css');
+}
+
+
