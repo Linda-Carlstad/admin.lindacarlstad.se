ff99cbd6f24f0c64289c82445cb779b2167ff9b0
Felix Wetell <felix.wetell@gmail.com>
2019-12-25 10:56:17 +0100

    Adds method to event model #56

--- app/Event.php
+++ app/Event.php
@@ -3,9 +3,41 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class Event extends Model
 {
     protected $table = 'events';
 
+    public static function create( Request $request )
+    {
+        Event::validateRequest( $request );
+        $event = new Event;
+        Event::addValuesToObject( $event, $request );
+    }
+
+    public static function updateInfo( Event $event, Request $request )
+    {
+        Event::validateRequest( $request );
+        Event::addValuesToObject( $event, $request );
+    }
+
+    public static function validateRequest( Request $request )
+    {
+        $request->validate( [
+            'title' => 'string|required',
+            'link' => 'string|nullable',
+            'link_title' => 'string|nullable',
+            'type' => 'string|nullable',
+        ] );
+    }
+
+    public static function addValuesToObject( Event $event, Request $request )
+    {
+        $event->title = $request->title;
+        $event->link = $request->link;
+        $event->link_title = $request->link_title;
+        $event->type = $request->type;
+        $event->save();
+    }
 }
