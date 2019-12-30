cff466f093894f20c67b1e64b6bd9e387c273f6a
Felix Wetell <felix.wetell@gmail.com>
2019-12-30 18:05:11 +0100

    Adds association views #43

---
+++ resources/views/association/edit.blade.php
@@ -0,0 +1,92 @@
+@extends('layouts.app')
+@section('content')
+
+    <div class="text-center">
+        <h2>{{ $association->title }}</h2>
+        <hr>
+    </div>
+
+    <form action="{{ '/association/' . $association->id }}" method="post"  enctype="multipart/form-data">
+        @csrf
+        {{ method_field( 'patch' ) }}
+        <div class="form-group row">
+            <label for="name">Namn</label>
+            <input id="name" type="text" placeholder="Namn" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ $association->name }}" required>
+
+            @if ($errors->has('name'))
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('name' ) }}</strong>
+                </span>
+            @endif
+        </div>
+        <div class="form-group row">
+            <label for="nickname">Kort namn</label>
+            <input id="nickname" type="text" placeholder="Kort namn" class="form-control{{ $errors->has('nickname') ? ' is-invalid' : '' }}" name="nickname" value="{{ $association->nickname }}">
+
+            @if ($errors->has('nickname'))
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('nickname' ) }}</strong>
+                </span>
+            @endif
+        </div>
+        <div class="form-group row">
+            <label for="image" class="">Bild</label>
+            <input type="file" class="filestyle" name="image" id="fileInput"
+                accept="image/*" data-text="Välj bild" data-btnClass="btn-primary btn-file">
+            @if ($errors->has('image'))
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('image' ) }}</strong>
+                </span>
+            @endif
+        </div>
+        <div class="form-group row">
+            <label for="link">Länk</label>
+            <input id="link" type="text" placeholder="Länk" class="form-control{{ $errors->has('link') ? ' is-invalid' : '' }}" name="link" value="{{ $association->link }}">
+
+            @if ($errors->has('link'))
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('link' ) }}</strong>
+                </span>
+            @endif
+        </div>
+        <div class="form-group row">
+            <label for="email">Kontakt - email</label>
+            <input id="email" type="email" placeholder="Email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ $association->email }}">
+
+            @if ($errors->has('email'))
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('email' ) }}</strong>
+                </span>
+            @endif
+        </div>
+        <div class="form-group row">
+            <label for="description">Beskrivning</label>
+            <textarea rows="3" id="description" type="text" placeholder="{{ $association->description }}" class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }}" name="description">{{ $association->description }}</textarea>
+
+            @if ($errors->has('description'))
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('description' ) }}</strong>
+                </span>
+            @endif
+        </div>
+        <div class="form-group row">
+            <a class="btn btn-secondary mr-1" href="{{ route( 'association.index' ) }}">Avbryt</a>
+            <button type="submit" class="btn btn-primary ml-0">
+                Spara
+            </button>
+        </div>
+    </form>
+    <hr>
+    <form onSubmit="return confirm('Är su säker på att du vill ta bort den här föreningen? Denna åtgärd är permanent.');" action="{{ '/association/' . $association->id }}" method="post">
+        @csrf
+        {{ method_field( 'delete' ) }}
+            <div class="text-center">
+                <h4>Ta bort förening</h4>
+                <p>Denna åtgärd är permanent.</p>
+                <button type="submit" class="btn btn-danger">
+                    Ta bort
+                </button>
+            </div>
+    </form>
+
+@endsection
---
+++ resources/views/association/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+@section('content')
+
+    <div class="text-center mb-2">
+        <h2>Föreningar</h2>
+        <hr>
+        @if( $associations->isEmpty() )
+            <p>
+                Inga föreningar tillagda, lägg till en nu!
+            </p>
+        @endif
+
+        <a class="btn btn-primary m-1" href="{{ route( 'association.create' ) }}">Lägg till förening</a>
+    </div>
+    @if( !$associations->isEmpty() )
+        <h4 class="text-center">Antal: {{ $associations->count() }}</h4>
+        <table class="table table-hover">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Position</th>
+                    <th>Namn</th>
+                    <th>Email</th>
+                    <th>#</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($associations as $association)
+                    <tr>
+                        <td>{{ $association->name }}</td>
+                        <td>{{ $association->nickname }}</td>
+                        <td>
+                            @if( $association->email )
+                                <a href="{{ $association->email }}">{{ $association->email }}</a>
+                            @else
+                                Ingen email
+                            @endif
+                        </td>
+                        <td>
+                            <a href="{{ url( 'association/' . $association->id . '/edit') }}" class="btn btn-link">
+                                Redigera
+                            </a>
+                        </td>
+                    </tr>
+
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+
+@endsection
