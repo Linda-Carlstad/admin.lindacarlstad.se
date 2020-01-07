fb49f633242f904efcf3dff69a33415b0f839031
Felix Wetell <felix.wetell@gmail.com>
2020-01-07 17:56:42 +0100

    Typo fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,9 @@
     <div class="text-center">
         <h2>Vad vill du göra idag?</h2>
         <p>Jag vill hantera...</p>
+        <a class="btn btn-primary m-1" href="{{ route( 'initiationHandle' ) }}">Nollningar</a>
         <a class="btn btn-primary m-1" href="{{ route( 'member.index' ) }}">Medlemmar</a>
         <a class="btn btn-primary m-1" href="{{ route( 'clothes' ) }}">Kläder</a>
-        <a class="btn btn-primary m-1" href="{{ route( 'initiationHandle' ) }}">Nollningen</a>
     </div>
 
 @endsection
--- resources/views/initiationHandle.blade.php
+++ resources/views/initiationHandle.blade.php
@@ -3,10 +3,8 @@
 
     <div class="text-center mb-2">
         <h2>Nollning</h2>
-        <p>
-            Nu vill jag hantera...
-        </p>
-        <a class="btn btn-primary m-1" href="{{ route( 'initiation.index' ) }}">Nollningar</a>
+        <p>Jag vill hantera...</p>
+        <a class="btn btn-primary m-1" href="{{ route( 'initiation.index' ) }}">Nollningsår</a>
         <a class="btn btn-primary m-1" href="{{ route( 'day.index' ) }}">Dagar</a>
         <a class="btn btn-primary m-1" href="{{ route( 'person.index' ) }}">Nyckelpersoner</a>
     </div>
